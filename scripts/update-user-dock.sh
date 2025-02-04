3cf3e14f438fd9d094164eeff82f654f69bc786a
Mike Matter <mike@fndtn.com>
2025-02-04 14:41:12 -0600

    Update update-user-dock.sh

--- scripts/update-user-dock.sh
+++ scripts/update-user-dock.sh
@@ -16,12 +16,12 @@ echo "------------------------------------------------------------------------"
 echo "Removing all Items from the Logged-In User's Dock..."
 sudo -u $loggedInUser $dockutil --remove all --no-restart $UserPlist
 echo "Creating New Dock..."
-sudo -u $loggedInUser $dockutil --add "'/Applications/Microsoft Outlook.app'" --no-restart $UserPlist
-sudo -u $loggedInUser $dockutil --add "'/Applications/Microsoft Teams.app'" --no-restart $UserPlist
-sudo -u $loggedInUser $dockutil --add "'/Applications/Company Portal.app'" --no-restart $UserPlist
-sudo -u $loggedInUser $dockutil --add "'/Applications/Microsoft Word.app'" --no-restart $UserPlist
-sudo -u $loggedInUser $dockutil --add "'/Applications/Microsoft Excel.app'" --no-restart $UserPlist
-sudo -u $loggedInUser $dockutil --add "'/Applications/Microsoft PowerPoint.app'" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Microsoft Teams.app" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Company Portal.app" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Microsoft Word.app" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Microsoft Excel.app" --no-restart $UserPlist
+sudo -u $loggedInUser $dockutil --add "/Applications/Microsoft PowerPoint.app" --no-restart $UserPlist
 sudo -u $loggedInUser $dockutil --add "System/Applications/System Preferences.app" --no-restart $UserPlist
 sudo -u $loggedInUser $dockutil --add "~/Downloads" --section others --view auto --display folder --no-restart $UserPlist
 echo "Restarting Dock..."
