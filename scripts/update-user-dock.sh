51aa31afb5f88e47668a347d1291a27f294c92db
Mike Matter <mike@fndtn.com>
2025-02-04 15:48:20 -0600

    Update update-user-dock.sh

--- scripts/update-user-dock.sh
+++ scripts/update-user-dock.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-dockutil="/usr/local/bin/dockutil"
-killall="/usr/bin/killall"
-loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
-LoggedInUserHome="/Users/$loggedInUser"
-UserPlist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
+dockutil=/usr/local/bin/dockutil
+currentUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
+uid=$(id -u "$currentUser")
+userHome=$(dscl . -read /users/${currentUser} NFSHomeDirectory | cut -d " " -f 2)
+plist="${userHome}/Library/Preferences/com.apple.dock.plist"
 
-# convenience function to run a command as the current user
-# usage:
-#   runAsUser command arguments...
 runAsUser() {  
   if [ "$currentUser" != "loginwindow" ]; then
     launchctl asuser "$uid" sudo -u "$currentUser" "$@"
   else
-    echo "no user logged in"
+    echo "No user logged in."
     # uncomment the exit command
     # to make the function exit with an error when no user is logged in
-    # exit 1
+    exitCode=1
+    exit $exitCode
   fi
 }
 
-################################################################################
-# Use Dockutil to Modify Logged-In User's Dock
-################################################################################
-
-echo "------------------------------------------------------------------------"
-echo "Current logged-in user: $loggedInUser"
-echo "------------------------------------------------------------------------"
-echo "Removing all Items from the Logged-In User's Dock..."
-runAsUser $dockutil --remove all --no-restart 
-echo "Creating New Dock..."
+# Configure the dock for a new user
 runAsUser $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart 
 runAsUser $dockutil --add "/Applications/Microsoft Teams.app" --no-restart 
 runAsUser $dockutil --add "/Applications/Company Portal.app" --no-restart 
 runAsUser $dockutil --add "/Applications/Microsoft Word.app" --no-restart 
 runAsUser $dockutil --add "/Applications/Microsoft Excel.app" --no-restart 
 runAsUser $dockutil --add "/Applications/Microsoft PowerPoint.app" --no-restart 
-runAsUser $dockutil --add "System/Applications/System Preferences.app" --no-restart 
-runAsUser $dockutil --add "~/Downloads" --section others --view auto --display folder --no-restart 
-echo "Restarting Dock..."
-runAsUser $killall Dock
+
+runAsUser $dockutil --remove "TV" --no-restart
+runAsUser $dockutil --remove "App Store" --no-restart
+runAsUser $dockutil --remove "Music" --no-restart
+runAsUser $dockutil --remove "Podcasts" --no-restart 
+runAsUser $dockutil --remove "FaceTime" --no-restart 
+runAsUser $dockutil --remove "Messages" --no-restart 
+runAsUser $dockutil --remove "News" --no-restart 
+runAsUser $dockutil --remove "Maps" --no-restart 
+runAsUser $dockutil --remove "Contacts" --no-restart 
+runAsUser $dockutil --remove "Freeform" --no-restart
+runAsUser $dockutil --remove "Reminders" --no-restart
+
+runAsUser $dockutil --move "System Settings" --position end
+
+/usr/bin/killall Dock
 
 exit 0
