8ee474c420c66760df74f6831dced1a34ae02011
Mike Matter <mike@fndtn.com>
2026-02-13 12:34:21 -0600

    Add files via upload

---
+++ scripts/LS/psso-video.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/dialog --title "Setting up Single Sign on" --video 'https://github.com/Foundation-Technologies/assets/raw/refs/heads/main/scripts/LS/Lovesac-PSSO.mov' --videocaption "This video shows you how to register your work Mac to enable Single Sign on" --autoplay
+
+exit 0
